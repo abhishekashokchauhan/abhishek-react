9f8dfd0361ba8d850553ebbe1ce1e481f149ce24
Abhishek Chauhan <abhishekashokchauhan@gmail.com>
2024-08-21 23:31:39 +0530

    Added react-router-dom and added routing and implemented the outlet

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Headers";
 import Body from "./components/Body";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 
 /**
  *
@@ -21,10 +25,32 @@ const AppLayout = () => {
   return (
     <div className="application">
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/body",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
---
+++ src/components/Error.js
@@ -0,0 +1,9 @@
+const Error = () => {
+  return (
+    <div>
+      <h1> My Error page </h1>
+    </div>
+  );
+};
+
+export default Error;
