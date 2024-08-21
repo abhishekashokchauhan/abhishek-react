ee043b85a3d516f57ef8e40851b47671e7587e9c
Abhishek Chauhan <abhishekashokchauhan@gmail.com>
2024-08-21 23:31:05 +0530

    Added 2 more components

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+const About = () => {
+  return (
+    <div>
+      <h1> Hello From the about page !</h1>
+      <h1> Hello From the about page !</h1>
+      <h1> Hello From the about page !</h1>
+      <h1> Hello From the about page !</h1>
+      <h1> Hello From the about page !</h1>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+const Contact = () => {
+  return (
+    <div>
+      <h1> Hello From the contact page !</h1>
+      <h1> Hello From the contact page !</h1>
+      <h1> Hello From the contact page !</h1>
+      <h1> Hello From the contact page !</h1>
+      <h1> Hello From the contact page !</h1>
+    </div>
+  );
+};
+
+export default Contact;
