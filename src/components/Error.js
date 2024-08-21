0dea8647377c544e19e9fd12814355498d89d5b3
Abhishek Chauhan <abhishekashokchauhan@gmail.com>
2024-08-21 23:33:46 +0530

    useRouteError hook implementation.

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,7 +1,14 @@
+import { useRouteError } from "react-router-dom";
+
 const Error = () => {
+  const error = useRouteError();
+  console.error(error);
+
   return (
     <div>
       <h1> My Error page </h1>
+      <h1> {error.status} </h1>
+      <h1> {error.message} </h1>
     </div>
   );
 };
